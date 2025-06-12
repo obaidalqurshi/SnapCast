25fce3df5424364296a1d23d7a752f2b365b022e
obaidallah <obaidalqershi1@gmail.com>
2025-06-12 23:13:38 +0300

    add arcjet protection

--- lib/actions/video.ts
+++ lib/actions/video.ts
@@ -7,6 +7,8 @@ import { BUNNY } from "@/constants"
 import { db } from "@/drizzle/db"
 import { videos } from "@/drizzle/schema"
 import { revalidatePath } from "next/cache"
+import aj from "../arcjet"
+import { fixedWindow, request } from "@arcjet/next"
 
 
 
@@ -28,6 +30,24 @@ const getSessionUserId = async (): Promise<string>=>{
 
 const revalidatePaths = (paths: string[]) =>{
     paths.forEach((path)=> revalidatePath(path))
+}
+
+const validateWithArcjet = async(fingerprint: string)=>{
+
+    const rateLimit = aj.withRule(
+        fixedWindow({
+            mode: 'LIVE',
+            window: '2m',
+            max: 2,
+            characteristics: ['fingerprint']
+        })
+    )
+
+    const req = await request();
+    const decision = await rateLimit.protect(req, {fingerprint});
+    if(decision.isDenied()){
+        throw new Error('Rate limit exceeded')
+    }
 }
 
 export const getVideoUploadUrl = withErrorHandling(async ()=>{
@@ -64,7 +84,7 @@ export const getThumbnailUploadUrl = withErrorHandling(async(videoId: string)=>{
 
 export const saveVideoDetials = withErrorHandling(async (videoDetails: VideoDetails)=>{
     const userId = await getSessionUserId();
-
+    await validateWithArcjet(userId)
     await apiFetch(`${VIDEO_STREAM_BASE_URL}/${BUNNY_LIBRARY_ID}/videos/${videoDetails.videoId}`,
         {
             method: 'POST',
---
+++ lib/arcjet.ts
@@ -0,0 +1,10 @@
+
+import arcjet from '@arcjet/next';
+import { getEnv } from './utils'
+
+const aj = arcjet({
+    key: getEnv('ARJECT_API_KEY'),
+    rules: [],
+})
+
+export default aj;
