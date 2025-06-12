f534b663c2d4e7132f5021a9df68e32c51de91ed
obaidallah <obaidalqershi1@gmail.com>
2025-06-13 00:03:36 +0300

    add server action to fetch all videos to the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,24 @@
 import Header from '@/components/Header'
 import VideoCard from '@/components/VideoCard'
 import { dummyCards } from '@/constants'
+import { getAllVideos } from '@/lib/actions/video'
 import React from 'react'
 
-const Page = () => {
+const Page = async ({ searchParams }: SearchParams) => {
+  const { query, filter, page } = await searchParams;
+
+  const { videos, pagination } = await getAllVideos(query, filter, Number(page) || 1);
+
   return (
     <main className='wrapper page'>
       <Header title='All Videos' subHeader='Public library' />
       <h1 className='text-2xl font-karla'> Welcome to Loom Clone</h1>
       <section className='video-grid'>
-        {dummyCards.map((card) => (
-          <VideoCard key={card.id} {...card} />
-        ))}
+        {videos?.length > 0 ? (
+          <section className='video-grid'>
+            {videos[0].video.title}
+          </section>
+        ) : (<div>empty</div>)}
       </section>
     </main>
   )
--- lib/actions/video.ts
+++ lib/actions/video.ts
@@ -2,13 +2,14 @@
 
 import { headers } from "next/headers"
 import { auth } from "../auth"
-import { apiFetch, getEnv, withErrorHandling } from "../utils"
+import { apiFetch, doesTitleMatch, getEnv, getOrderByClause, withErrorHandling } from "../utils"
 import { BUNNY } from "@/constants"
 import { db } from "@/drizzle/db"
-import { videos } from "@/drizzle/schema"
+import { user, videos } from "@/drizzle/schema"
 import { revalidatePath } from "next/cache"
 import aj from "../arcjet"
 import { fixedWindow, request } from "@arcjet/next"
+import { and, eq, or, sql } from "drizzle-orm"
 
 
 
@@ -32,6 +33,15 @@ const revalidatePaths = (paths: string[]) =>{
     paths.forEach((path)=> revalidatePath(path))
 }
 
+const buildVideoWithUserQuery = ()=>{
+    return db
+        .select({
+            video: videos,
+            user: {id: user.id, name: user.name, image: user.image}
+        })
+        .from(videos)
+        .leftJoin(user, eq(videos.userId, user.id))
+}
 const validateWithArcjet = async(fingerprint: string)=>{
 
     const rateLimit = aj.withRule(
@@ -108,4 +118,48 @@ export const saveVideoDetials = withErrorHandling(async (videoDetails: VideoDeta
  return{
     videoId: videoDetails.videoId
  }
+})
+export const getAllVideos = withErrorHandling(async(searchQuery: string = ''
+    ,sortFilter?: string,
+    pageNumber: number =1,
+    pageSize: number =8
+)=>{
+
+    const session = await auth.api.getSession({headers: await headers()} )
+    const currentUserId  = session?.user.id;
+    const canSeeTheVideos = or(
+        eq(videos.visibility, 'public'),
+        eq(videos.userId, currentUserId!),
+
+    );
+    const whereCondition = searchQuery.trim() 
+        ? and(
+            canSeeTheVideos,
+            doesTitleMatch(videos, searchQuery)
+        )
+        : canSeeTheVideos
+
+    const [{totalCount}] = await db.select(
+        {totalCount: sql<number>`count(*)`}
+    ).from(videos)
+    .where(whereCondition)
+
+    const totalVideos = Number(totalCount || 0);
+    const totalPages = Math.ceil(totalVideos/pageSize);
+
+    const videoRecords = await buildVideoWithUserQuery()
+        .where(whereCondition)
+        .orderBy(sortFilter ? getOrderByClause(sortFilter): sql`${videos.createdAt} DESC`)
+        .limit(pageSize)
+        .offset((pageNumber -1) * pageSize)
+
+    return {
+        videos: videoRecords,
+        pagination: {
+            currentPage: pageNumber,
+            totalPages,
+            totalVideos,
+            pageSize
+        }
+    }
 })
